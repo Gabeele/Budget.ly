c465aa3b500467dd2a15eed66bbfada34eeff138
Gavin Abeele <abeele.s.gavin@gmail.com>
2022-03-28 12:59:33 -0400

    Add project files.

---
+++ Budget.ly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Budget.ly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
