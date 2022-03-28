b5c5fd1251b6a833c13ffe1535f7e8e9b347deb7
Karl Stencell <karldstencell@gmail.com>
2022-03-28 15:40:15 -0400

    Added Items wrapper for item list, aggregate interface, iterator interface, and ItemIterator class

---
+++ Budget.ly/ItemIterator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget.ly
+{
+    class ItemIterator : Iterator
+    {
+        private List<Item> elements;
+        private int index = 0;
+
+        public ItemIterator(Items items)
+        {
+            elements = new List<Item>(items.GetItems());
+        }
+
+        public bool hasNext()
+        {
+            return index < elements.Count();
+        }
+
+        public object next()
+        {
+            if (hasNext())
+            {
+                return elements.ElementAt(index++);
+            }
+            return null;
+        }
+    }
+}
---
+++ Budget.ly/Iterator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget.ly
+{
+    interface Iterator
+    {
+        bool hasNext();
+
+        Object next();
+    }
+}
