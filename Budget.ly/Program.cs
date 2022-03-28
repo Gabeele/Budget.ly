fda017dad04f559f0d53f9bd5b1145330867ab78
Gavin Abeele <abeele.s.gavin@gmail.com>
2022-03-28 13:17:07 -0400

    Pushing UI

--- Budget.ly/Program.cs
+++ Budget.ly/Program.cs
@@ -6,7 +6,7 @@ namespace Budget.ly
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
---
+++ Budget.ly/UI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget.ly
+{
+    class UI
+    {
+        private Account account;
+
+       public UI()
+        {
+            this.account = null;
+
+        }
+
+
+
+
+
+    }
+}
