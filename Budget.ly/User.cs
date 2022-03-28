f10ff2ba4e2f2e6f542d87947f2c9c5f0d2b3688
bbowler <bbowler1740@conestogac.on.ca>
2022-03-28 13:15:04 -0400

    Created initial Account class.

---
+++ Budget.ly/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget.ly
+{
+    public class User
+    {
+
+        string firstName;
+        string lastName;
+
+        public User(string firstName, string lastName)
+        {
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+
+        }
+
+
+
+
+    }
+}
