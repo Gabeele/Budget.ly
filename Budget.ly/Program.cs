3cf3366e5f8bb6a8986269ad2104e61dedc9a392
Gavin Abeele <abeele.s.gavin@gmail.com>
2022-03-28 14:51:55 -0400

    Outline for the UI

--- Budget.ly/Program.cs
+++ Budget.ly/Program.cs
@@ -6,7 +6,8 @@ namespace Budget.ly
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UI ui = new UI();
+            ui.selectAccount();
         }
     }
 }
